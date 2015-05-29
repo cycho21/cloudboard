061ae528d29e1c4a7988513fdbcdd7c330241543
lovebube <lovebube@hotmail.com>
2015-05-29 15:19:06 +0900

    052915 15:20 /*	getStatements(), getCodeBlock() 			  * 따로 빼내서 오브젝트화 하는 것이 좋은지?

--- src/kr/ac/uos/ai/cloudBoard/astNode/IfStatement.java
+++ src/kr/ac/uos/ai/cloudBoard/astNode/IfStatement.java
@@ -1,7 +1,15 @@
 package kr.ac.uos.ai.cloudBoard.astNode;
 
+import java.util.LinkedList;
+
 import lombok.Data;
 
-public @Data class IfStatement {
-
+public @Data class IfStatement extends StatementCode {
+	
+	private ExpressionCode					condition;				
+	private LinkedList<StatementCode>		codeBlock;
+	
+	public IfStatement() {
+		codeBlock = new LinkedList<StatementCode>();
+	}
 }
